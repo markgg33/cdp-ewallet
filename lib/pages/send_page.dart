39e57ad5a104b23088e479d8c647b0685ef89da9
markgg33 <siopaomark@gmail.com>
2023-06-02 13:36:11 +0800

    Added contacts and textfield for send_page

--- lib/pages/send_page.dart
+++ lib/pages/send_page.dart
@@ -13,53 +13,149 @@ class _SendPageState extends State<SendPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.indigoAccent[200],
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //APP BAR SECTION STARTS
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.indigoAccent[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Padding(
-                padding: const EdgeInsets.only(top: 110),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap:() {
-                        Navigator.pop(context);
-                      },
-                      child: Icon(
-                        Icons.arrow_back_ios_new_rounded,
-                        color: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 110),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0,0,130,0),
-                      child: Row(
-                        children: [
-                          Text(
-                            "SEND",
-                            style: TextStyle(
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 130, 0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "SEND",
+                              style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white,
-                                fontSize: 30,),
-                          ),
-                        ],
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                          //APP BAR SECTION ENDS
+                        ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.grey[400]),
+                      child: Icon(
+                        Icons.person,
+                        size: 80,
+                        color: Colors.grey[600],
+                      )),
+                  Container(
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.grey[400]),
+                      child: Icon(
+                        Icons.person,
+                        size: 80,
+                        color: Colors.grey[600],
+                      )),
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.grey[400]),
+                    child: Icon(
+                      Icons.person,
+                      size: 80,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 30, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'RECIPIENT:',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 20,
+                    ),
+                    child: Icon(
+                      Icons.perm_contact_cal,
+                      size: 40,
+                      color: Colors.grey[400],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Recipient',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 30, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'AMOUNT:',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Please set an amount',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
